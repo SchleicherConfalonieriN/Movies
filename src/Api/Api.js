bfa44ea57c57812048e70aeb56de0194f1f54b90
Nicolas Schleicher <nicosc41@gmail.com>
2022-06-23 17:37:52 -0300

    <feat> add search api

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -3,6 +3,11 @@ import Config from './Config.js'
 const Api = { 
    
    genre: Config.url+'/genre/movie/list?api_key='+Config.key+'&language=en-US',
+   search:{
+      searchS: Config.url+'search/movie?api_key='+Config.key+'&language=en-US&query=',
+      searchE:'&page=1&include_adult=false',
+   },
+
    trendingweek: 'https://api.themoviedb.org/3/trending/movie/week?api_key=',
    discoverP : Config.url+'discover/movie?api_key='+Config.key+'&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate'
 }
