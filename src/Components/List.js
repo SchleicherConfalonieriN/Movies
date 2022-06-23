1132cd0d17d178906a7752e21a6851355a6213e3
Nicolas Schleicher <nicosc41@gmail.com>
2022-06-23 17:40:06 -0300

    <fix> add dependencis.. to useEffect

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,32 +1,31 @@
 import React,{useEffect,useState} from 'react'
 import Movie from './Movie.js'
 import axios from 'axios'
+
 const List = (props) => {
+    
+    let Api = props.Api;
 
     const[list, setList] = useState([])
     useEffect(()=>{
-        getData()
-    },[])
-
+        getData();
+    
+    },[Api])
     const getData = async () =>{
-      const res = await axios.get(props.Api)
-      console.log(res)
-      setList(res.data.results) 
+      const res = await axios.get(Api)
+      setList(res.data.results)
+
     }
-
-
-
     return (
         <div className="ListContainer">
             {list.map((movie)=>
-            <Movie 
+            <Movie key={movie.id}
             poster={movie.poster_path}
             title={movie.original_title}
             overview={movie.overview}
             realese={movie.release_date}
             popularity={movie.popularity}
             />
-            
             )}
         </div>
     )
