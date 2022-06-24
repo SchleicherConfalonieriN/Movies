f203023ba06c12e5b1f11c5686133b822a9b7741
Nicolas Schleicher <nicosc41@gmail.com>
2022-06-24 08:43:00 -0300

    <feat> add stars and input radio for rating filter

--- src/Main/SearchList.js
+++ src/Main/SearchList.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import List from '../Components/List.js'
 import Api from '../Api/Api.js'
+import {AiFillStar} from 'react-icons/ai'
 
 
 const SearchList = (props) =>{
+
+    const [rating, setRating] = useState("")
+
+
+
     const Url= Api.search.searchS + props.Query + Api.search.searchE
         return ( 
             <div>
+                {[...Array(5)].map((star) => {
+                    return(
+                    <label>
+                        <input type= "radio" name="rating"></input>    
+                        <AiFillStar  size={50}/>
+                    </label>
+                    );})}
+
             <List Api={Url}/>
             </div>)
         }
